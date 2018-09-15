a2b14e35c36dcc6afff48eb76fa11d8d1900272e
root <root@vps526290.ovh.net>
2018-09-15 21:40:02 +0200

    mise au point multivolume

--- backup/ftpsync.sh
+++ backup/ftpsync.sh
@@ -10,7 +10,7 @@ fi
 BACKUP_FTP_HOST="inconnu.ftp"
 BACKUP_FTP_PATH="/backup"
 BACKUP_FTP_LOGIN="userFtp"
-BACKUP_FTP_PASSWORD="userPwd"
+BACKUP_FTP_PWD="userPwd"
 APP_SRC_PATH="/var/data"
 APP_VOLUMES=('vol1' 'vol2')
 EXCLUDED="*.*~"
@@ -35,7 +35,7 @@ then
    fi   
    if [ ! -z $3 ]
    then
-      BACKUP_FTP_PASSWORD=$3
+      BACKUP_FTP_PWD=$3
    fi
    if [ ! -z $4 ]
    then
@@ -57,19 +57,21 @@ fi
 echo BACKUP_FTP_HOST=$BACKUP_FTP_HOST
 echo BACKUP_FTP_PATH=$BACKUP_FTP_PATH
 echo BACKUP_FTP_LOGIN=$BACKUP_FTP_LOGIN
+#echo BACKUP_FTP_PWD=$BACKUP_FTP_PWD
 echo APP_SRC_PATH=$APP_SRC_PATH
 echo APP_VOLUMES=${APP_VOLUMES[*]}
 echo BACKUP_TYPE=$BACKUP_TYPE
 echo -e "Init param finished\n"
 
-echo "backup $APP_SRC_PATH on $BACKUP_FTP_LOGIN@$BACKUP_FTP_HOST:$BACKUP_FTP_PATH"
 
 for volume in ${APP_VOLUMES[@]}
 do 
+  echo "backup $APP_SRC_PATH/$volume sur $BACKUP_FTP_LOGIN@$BACKUP_FTP_HOST/$BACKUP_FTP_PATH/$BACKUP_TYPE";
   lftp -c "set ftp:ssl-allow no;
   open ftp://$BACKUP_FTP_LOGIN:$BACKUP_FTP_PWD@$BACKUP_FTP_HOST; 
+  mkdir -pf $BACKUP_FTP_PATH/$BACKUP_TYPE/$volume;
   lcd $APP_SRC_PATH/$volume;
-  cd $BACKUP_FTP_PATH/$volume;
+  cd $BACKUP_FTP_PATH/$BACKUP_TYPE/$volume;
   mirror --reverse \
          --delete \
          --verbose \
